6dcca514e7f7d4bd671791c3b2c7caeb7547130b
James Katarikawe <jpkat92@gmail.com>
2018-04-24 15:13:47 +0300

    All tests written and failing

--- src/app.py
+++ src/app.py
@@ -1,3 +1,9 @@
 # this python function  returns a list containing sequence of prime numbers below the given argument
 def prime_num_generator(end):
     pass
+
+def is_prime(num):
+     #Return true if *num* is prime.
+    pass
+
+       
--- src/tests.py
+++ src/tests.py
@@ -1,5 +1,6 @@
 import unittest
-from app import prime_num_generator
+from app import prime_num_generator, is_prime
+import random
 
 # Test cases: A prime number is a natural number that is only divisible by 2 and itself
 # check that list of integers is returned
@@ -8,7 +9,10 @@ class MainTest(unittest.TestCase):
     def test_prime_num_generator(self):
        self.assertEqual(type(prime_num_generator(100)), list, msg="A list must be returned")
        self.assertTrue(isinstance(prime_num_generator(100)[-1], int), msg="Error: Integers must be written")
-       self.assertEqual(prime_num_generator(100)[-2]>1)
+       self.assertTrue(prime_num_generator(100)[-2]>1, msg="A prime number cannot be less than 2")
+       length = len(prime_num_generator(100))
+       self.assertTrue(is_prime(random.choice(prime_num_generator(100))), msg="Number is not a prime number")
+
 
 if __name__ == '__main__':
     unittest.main()
