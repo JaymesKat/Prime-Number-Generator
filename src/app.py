825c7071b75524394c992062af2a79ae03ac2432
James Katarikawe <jpkat92@gmail.com>
2018-04-24 15:16:34 +0300

    Prime Number Generator Function Implemented and all tests pass

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
 # this python function  returns a list containing sequence of prime numbers below the given argument
 def prime_num_generator(end):
-    pass
+    noprimeNums = set(j for i in range(2, 8) for j in range(i*2, end, i))      
+    primeNums = [x for x in range(2, end) if x not in noprimeNums]
+    return primeNums
 
 def is_prime(num):
      #Return true if *num* is prime.
-    pass
+    if num <= 1:
+        return False
+
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
 
        
